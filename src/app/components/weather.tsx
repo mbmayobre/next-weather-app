ee593c20f0d3c5ddae2dbc04980ac4cf45d8e8f1
michael mayobre <mbmayobre@gmail.com>
2025-03-18 15:35:04 -0400

    fixed search icon and clear button

--- src/app/components/weather.tsx
+++ src/app/components/weather.tsx
@@ -83,17 +83,21 @@ export default function Weather() {
     <div className="flex flex-col items-center space-y-4 p-4">
       <div className="w-full max-w-sm min-w-[300px]">
         <div className="relative flex items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="absolute w-5 h-5 top-2.5 left-2.5 text-slate-600">
-            <path fill-rule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clip-rule="evenodd" />
-          </svg>
-      
+          <FiSearch size={30} className="absolute w-7 h-7 pl-3 text-slate-600" />
           <input
             className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md pl-10 pr-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
             placeholder="Enter city name or zip code..." 
             value={city}
             onChange={(e) => setCity(e.target.value)}
           />
-          
+          {city.length > 0 && 
+            <button
+              onClick={() => setCity("")}
+              className="absolute right-24 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
+            >
+              <IoMdClose size={20} />
+            </button>
+          }  
           <button
             onClick={fetchLocation}
             className="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
