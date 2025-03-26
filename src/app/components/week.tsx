e7c096914f5c5da831d0c4f9615cc2e233d828c2
michael mayobre <mbmayobre@gmail.com>
2025-03-26 18:47:23 -0400

    created base week component

---
+++ src/app/components/week.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { FunctionComponent, useEffect, useState, useCallback } from "react";
+import { weather } from "../lib/definitions";
+
+interface WeekProps {
+  weather: weather;
+}
+
+export const WeekWeather: FunctionComponent<WeekProps> = ({ weather }) => {
+
+  return (
+    <></>
+  );
+};
