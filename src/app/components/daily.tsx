0f0df017b67c2477b10f0567dbc512d04e039f8f
michael mayobre <mbmayobre@gmail.com>
2025-03-27 21:26:12 -0400

    fixed styling for daily weather

--- src/app/components/daily.tsx
+++ src/app/components/daily.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { FunctionComponent, useEffect, useState, useCallback } from "react";
+import { FunctionComponent } from "react";
 import { weather } from "../lib/definitions";
 import { MdOutlineCalendarToday } from "react-icons/md";
 
@@ -33,18 +33,18 @@ export const DailyWeather: FunctionComponent<DailyWeatherProps> = ({ weather }) 
         <MdOutlineCalendarToday size={25} className="font-bold" />
         <p className="ml-3 font-semibold">Hourly Forecast</p>
       </div>
-      <div className="flex flex-row overflow-x-scroll pb-2">
+      <div className="flex flex-row justify-center overflow-x-auto pb-2 w-full">
         {weather.daily.map((day, index) => (
-          <div key={index} className="flex flex-col justify-center bg-gray-200 w-auto" >
-            <p>{Math.round(day.temp.max)}°</p>
-            <p>{Math.round(day.temp.min)}°</p>
+          <div key={index} className="flex flex-col mx-2 pt-2 pb-2 sm:pb-5 px-3 bg-gray-200 rounded-full h-full max-w-min" >
+            <p className="text-center">{Math.round(day.temp.max)}°</p>
+            <p className="text-center">{Math.round(day.temp.min)}°</p>
             <img
               className="flex w-full h-full object-cover my-2"
               src={`https://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
               alt="Daily Weather Icon"
             />
-            <p>{index === 0 ? 'Today' : formatDayAndDate(day.dt).day}</p>
-            <p>{formatDayAndDate(day.dt).date}</p>
+            <p className="text-center mx-auto w-10">{index === 0 ? 'Today' : formatDayAndDate(day.dt).day}</p>
+            <p className="text-center">{formatDayAndDate(day.dt).date}</p>
           </div>
         ))}
       </div>
