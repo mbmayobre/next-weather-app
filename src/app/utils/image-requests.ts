1c438bc53afcc80c25b9ca901636ab78fe7adf2b
michael mayobre <mbmayobre@gmail.com>
2025-06-19 11:46:40 -0400

    created icon request functions

--- src/app/utils/image-requests.ts
+++ src/app/utils/image-requests.ts
@@ -1,4 +1,4 @@
-import { Background } from './dictionary';
+import { Background, WeatherIcon } from './dictionary';
 
 export function getBackgroundFromCode(code: number): Background {
   switch (true) {
@@ -19,8 +19,57 @@ export function getBackgroundFromCode(code: number): Background {
   }
 }
 
+export function getIconFromCode(code: number): WeatherIcon {
+  switch (true) {
+    case code === 200 && code < 210:
+      return 'thunder-and-rain';
+    case code === 210:
+      return 'light-thunder';
+    case code === 211 || code === 221:
+      return 'thunder';
+    case code === 212:
+      return 'heavy-thunder';
+    case code === 230 || code === 231 || code === 232:
+      return 'thunder-and-rain';
+    case code >= 300 && code <= 321:
+      return 'drizzle';
+    case code === 500 || code === 520:
+      return 'light-rain';
+    case code === 501 || code === 521:
+      return 'rain';
+    case code >= 502 && code <= 504:
+      return 'heavy-rain';
+    case code === 511 || code === 611 || code === 612 || code === 613:
+      return 'freezing-rain';
+    case code === 522 || code === 531:
+      return 'heavy-rain';
+    case code === 600 || code === 620:
+      return 'light-snow';
+    case code === 601 || code === 621:
+      return 'snow';
+    case code === 602 || code === 622:
+      return 'heavy-snow';
+    case code === 615 || code === 616:
+      return 'rain-and-snow';
+    case code >= 701 && code <= 781:
+      return 'mist';
+    case code === 800:
+      return 'clear-day';
+    case code === 801:
+      return 'few-clouds-day';
+    case code === 802:
+      return 'cloudy-day';
+    case code === 803:
+      return 'cloudy';
+    case code === 804:
+      return 'very-cloudy';
+    default:
+      return 'clear-day';
+  }
+}
+
 export function getBackgroundFromIcon(icon: string, code: number): Background {
-  const isDay   = icon.endsWith('d');
+  const isDay = icon.endsWith('d');
 
   // clear
   if (code === 800) {
@@ -34,4 +83,26 @@ export function getBackgroundFromIcon(icon: string, code: number): Background {
 
   // for everything else, just fall back to the existing mapping
   return getBackgroundFromCode(code);
+}
+
+export function getIconFromIcon(icon: string, code: number): WeatherIcon {
+  const isDay = icon.endsWith('d');
+
+  // clear
+  if (code === 800) {
+    return isDay ? 'clear-day' : 'clear-night';
+  }
+
+  // few clouds
+  if (code === 801) {
+    return isDay ? 'few-clouds-day' : 'few-clouds-night';
+  }
+
+  // cloudy
+  if (code === 802) {
+    return isDay ? 'cloudy-day' : 'cloudy-night';
+  }
+
+  // for everything else, just fall back to the existing mapping
+  return getIconFromCode(code);
 }
