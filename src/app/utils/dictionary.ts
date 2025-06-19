1ed421f319a7bb4efe306b7d9da44094dc046f3f
michael mayobre <mbmayobre@gmail.com>
2025-06-19 11:08:45 -0400

    fix weather icon names

--- src/app/utils/dictionary.ts
+++ src/app/utils/dictionary.ts
@@ -11,6 +11,30 @@ export type Background =
   | 'cloudy'
   | 'home';
 
+export type WeatherIcon =
+  | 'clear-day'
+  | 'clear-night'
+  | 'cloudy'
+  | 'cloudy-day'
+  | 'cloudy-night'
+  | 'drizzle'
+  | 'few-clouds-night'
+  | 'few-clouds-day'
+  | 'freezing-rain'
+  | 'heavy-rain'
+  | 'heavy-snow'
+  | 'heavy-thunder'
+  | 'light-rain'
+  | 'light-snow'
+  | 'light-thunder'
+  | 'mist'
+  | 'rain-and-snow'
+  | 'rain'
+  | 'snow'
+  | 'thunder-and-rain'
+  | 'thunder'
+  | 'very-cloudy'
+
 export const bgClassMap: Record<Background, string> = {
   'thunderstorm':        'bg-thunderstorm',
   'light-rain':          'bg-light-rain',
@@ -23,7 +47,7 @@ export const bgClassMap: Record<Background, string> = {
   'partly-cloudy-night': 'bg-partly-cloudy-night',
   'cloudy':              'bg-cloudy',
   'home':                'bg-home',
-}
+} 
 
 export function getBackgroundFromCode(code: number): Background {
   switch (true) {
