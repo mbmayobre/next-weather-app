ff60b1e26a080c20fd0cbf7c7716d907c22167e8
Michael <mbmayobre@gmail.com>
2025-06-24 10:18:10 -0400

    fixed styling for wind direction

--- src/app/components/wind.tsx
+++ src/app/components/wind.tsx
@@ -2,7 +2,6 @@
 
 import { FunctionComponent } from "react";
 import { weather } from "../lib/definitions";
-import { LiaArrowAltCircleUpSolid } from "react-icons/lia";
 import { FaWind } from "react-icons/fa6";
 
 interface WindProps {
@@ -17,18 +16,25 @@ export const Wind: FunctionComponent<WindProps> = ({ weather }) => {
   };
 
   return (
-    <div className="flex flex-col mr-4 bg-gray-200 dark:bg-opacity-40 bg-opacity-40 text-black dark:bg-black dark:text-white w-full h-[175px] rounded-2xl mt-4 p-4">
-      <div className="flex flex-row justify-start ml-2 mt-2 mb-2">
+    <div 
+      className="relative flex flex-col mr-4 bg-gray-200 dark:bg-opacity-40 bg-opacity-40 text-black dark:bg-black dark:text-white w-full h-[175px] rounded-2xl mt-4 p-4"
+    >
+      <div className="flex z-[2] flex-row justify-start ml-2 mt-2 mb-2">
         <FaWind size={25} />
         <p className="ml-3 font-semibold">Wind</p>
       </div>
-      <div className="flex flex-row justify-start items-center ml-2 h-full">
+      <div className="flex z-[2] flex-row justify-start items-center ml-2 h-full">
         <p className="text-2xl font-semibold">{weather.current.wind_speed}mph</p>
       </div>
-      <div className="flex flex-row justify-start items-center ml-2 h-full">
-        <LiaArrowAltCircleUpSolid size={30} style={{ transform: `rotate(${weather.current.wind_deg + 180}deg)` }} className='font-bold' />
+      <div className="flex z-[2] flex-row justify-start items-center ml-2 h-full">
         <p className="text-xs ml-4 text-gray-800 dark:text-gray-400">From {convertDegreesToDirection(weather.current.wind_deg)}</p>
       </div>
+      <img
+        src="/icons/arrow.svg"
+        alt="Wind Direction"
+        className="absolute z-[1] opacity-75 w-[125px] h-[125px] inset-y-auto inset-x-auto self-center"
+        style={{ transform: `rotate(${weather.current.wind_deg + 180}deg)` }}
+      />
     </div>
   );
 };
