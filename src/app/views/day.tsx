092723ad78e236d203b970f5aa4ba98616c17f01
michael mayobre <mbmayobre@gmail.com>
2026-05-28 18:32:49 -0400

    created base file for day view

---
+++ src/app/views/day.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import { FunctionComponent, useEffect, useState } from "react";
+
+interface DayProps {}
+
+export const Day: FunctionComponent<DayProps> = ({}) => {
+};
+
+export default Day;
